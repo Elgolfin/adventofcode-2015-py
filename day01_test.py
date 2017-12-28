d3134871d75732b6ebb42061377085f33f9021b7
Elgolfin <elgo@nicomputer.net>
2017-12-28 10:34:49 -0500

    Solve the day 1 puzzles

---
+++ day01_test.py
@@ -0,0 +1,16 @@
+"""Day 01 unit tests"""
+
+import unittest
+from day01 import walk_through_floors
+
+class Day01TestCase(unittest.TestCase):
+    """Tests for `day01.py`"""
+
+    def test_(self):
+        """Tests for the final floor"""
+        floor, enter_basement_at = walk_through_floors("(()))")
+        self.assertEqual(floor, -1)
+        self.assertEqual(enter_basement_at, 5)
+
+if __name__ == '__main__':
+    unittest.main()
